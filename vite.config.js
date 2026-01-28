beef1e55baa99bdbed4ddc1cc2cd9c72574fa7e8
Mohammad-Wahid-Albadawi <mohammadwahidalbadawi@gmail.com>
2026-01-28 13:03:48 +0300

    update pwa options

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-        react() ,
-        VitePWA({ registerType: 'autoUpdate' })
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Prayer Times',
+        short_name: 'PT',
+        description: 'Show prayer times for most capitals in arab world',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          }
+        ],
+      },
+    }),
   ],
 })
