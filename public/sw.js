3ffbb010b0ad6205c6176f183136f349a66d200f
Mohammad-Wahid-Albadawi <mohammadwahidalbadawi@gmail.com>
2026-02-05 12:03:29 +0300

    cache background image

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,7 @@ this.addEventListener("install", (event) => {
         '/static/js/main.chunk.js',
         '/static/js/0.chunk.js',
         '/static/js/bundle.js',
+        '/assets/photo_2024-10-04_19-07-28.jpg',
         '/index.html',
         '/'
       ])
