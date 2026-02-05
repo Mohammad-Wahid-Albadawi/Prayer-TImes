ffb16cb98d0e80d99a6ac5930eb93c54ce662c1b
Mohammad-Wahid-Albadawi <mohammadwahidalbadawi@gmail.com>
2026-02-05 15:31:12 +0300

    s

--- public/sw.js
+++ public/sw.js
@@ -3,10 +3,10 @@ this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
+        '/photo_2024-10-04_19-07-28.jpg',
         '/static/js/main.chunk.js',
         '/static/js/0.chunk.js',
         '/static/js/bundle.js',
-        '/photo_2024-10-04_19-07-28.jpg',
         '/index.html',
         '/'
       ])
@@ -15,13 +15,9 @@ this.addEventListener("install", (event) => {
 })
 
 this.addEventListener("fetch", (event) => {
-    if(!navigator.onLine) {
-      event.respondWith(
-      caches.match(event.request).then((resp) => {
-      if (resp) {
-        return resp
-      }
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
     })
-  )
-}
-})
+  );
+});
